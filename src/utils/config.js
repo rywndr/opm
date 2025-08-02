82770c02a1758de20bfec087b3bd9fda654a0e6b
sudodot <haikhalroy@gmail.com>
2025-08-02 13:15:54 +0700

    Moved consumer app to be a standalone app

--- src/utils/config.js
+++ src/utils/config.js
@@ -21,12 +21,6 @@ const config = {
   redis: {
     host: process.env.REDIS_SERVER,
   },
-  mail: {
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    user: process.env.SMTP_USER,
-    password: process.env.SMTP_PASSWORD,
-  },
 };
 
 module.exports = config;
